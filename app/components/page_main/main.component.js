68f315cc3a5f9aa9060a1d430159f21579423c48
Webkingdev <webking.dev2@gmail.com>
2016-09-30 13:01:47 +0300

    Инициализация svg

--- app/components/page_main/main.component.js
+++ app/components/page_main/main.component.js
@@ -10,11 +10,65 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var router_1 = require('@angular/router');
+var core_2 = require('@angular/core');
 var MainComponent = (function () {
-    function MainComponent(router) {
+    function MainComponent(router, elRef) {
         this.router = router;
+        this.elRef = elRef;
     }
     MainComponent.prototype.ngOnInit = function () {
+        jQuery(this.elRef.nativeElement).find('img.svg').each(function () {
+            var $img = jQuery(this);
+            var imgID = $img.attr('id');
+            var imgClass = $img.attr('class');
+            var imgURL = $img.attr('src');
+            jQuery.get(imgURL, function (data) {
+                // Get the SVG tag, ignore the rest
+                var $svg = jQuery(data).find('svg');
+                // Add replaced image's ID to the new SVG
+                if (typeof imgID !== 'undefined') {
+                    $svg = $svg.attr('id', imgID);
+                }
+                // Add replaced image's classes to the new SVG
+                if (typeof imgClass !== 'undefined') {
+                    $svg = $svg.attr('class', imgClass + ' replaced-svg');
+                }
+                // Remove any invalid XML tags as per http://validator.w3.org
+                $svg = $svg.removeAttr('xmlns:a');
+                // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+                if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                    $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'));
+                }
+                // Replace image with new SVG
+                $img.replaceWith($svg);
+            }, 'xml');
+        });
+        jQuery(this.elRef.nativeElement).find('svg use').each(function () {
+            var $img = jQuery(this);
+            var imgID = $img.attr('id');
+            var imgClass = $img.attr('class');
+            var imgURL = $img.attr('xlink:href');
+            jQuery.get(imgURL, function (data) {
+                // Get the SVG tag, ignore the rest
+                var $svg = jQuery(data).find('svg');
+                // Add replaced image's ID to the new SVG
+                if (typeof imgID !== 'undefined') {
+                    $svg = $svg.attr('id', imgID);
+                }
+                // Add replaced image's classes to the new SVG
+                if (typeof imgClass !== 'undefined') {
+                    $svg = $svg.attr('class', imgClass + ' replaced-svg');
+                }
+                // Remove any invalid XML tags as per http://validator.w3.org
+                $svg = $svg.removeAttr('xmlns:a');
+                // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+                if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                    $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'));
+                }
+                // Replace image with new SVG
+                $img.replaceWith($svg);
+            }, 'xml');
+        });
     };
     MainComponent = __decorate([
         core_1.Component({
@@ -22,7 +76,7 @@ var MainComponent = (function () {
             selector: 'my-main',
             templateUrl: './main.component.html'
         }), 
-        __metadata('design:paramtypes', [router_1.Router])
+        __metadata('design:paramtypes', [router_1.Router, core_2.ElementRef])
     ], MainComponent);
     return MainComponent;
 }());
--- app/components/page_main/main.component.ts
+++ app/components/page_main/main.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router }            from '@angular/router';
 
+import { ElementRef } from '@angular/core';
+declare var jQuery: any;
+
 @Component({
 	moduleId: module.id,
 	selector: 'my-main',
@@ -8,9 +11,74 @@ import { Router }            from '@angular/router';
 })
 
 export class MainComponent implements OnInit{
-	constructor(private router: Router) {
+	constructor(private router: Router, private elRef: ElementRef) {
 	}
-	ngOnInit(): void{
+	ngOnInit(): any{
+		jQuery(this.elRef.nativeElement).find('img.svg').each(function(){
+			var $img = jQuery(this);
+			var imgID = $img.attr('id');
+			var imgClass = $img.attr('class');
+			var imgURL = $img.attr('src');
 
+			jQuery.get(imgURL, function(data) {
+				// Get the SVG tag, ignore the rest
+				var $svg = jQuery(data).find('svg');
+
+				// Add replaced image's ID to the new SVG
+				if(typeof imgID !== 'undefined') {
+					$svg = $svg.attr('id', imgID);
+				}
+				// Add replaced image's classes to the new SVG
+				if(typeof imgClass !== 'undefined') {
+					$svg = $svg.attr('class', imgClass+' replaced-svg');
+				}
+
+				// Remove any invalid XML tags as per http://validator.w3.org
+				$svg = $svg.removeAttr('xmlns:a');
+
+				// Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+				if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+					$svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+				}
+
+				// Replace image with new SVG
+				$img.replaceWith($svg);
+
+			}, 'xml');
+
+		});
+		jQuery(this.elRef.nativeElement).find('svg use').each(function(){
+			var $img = jQuery(this);
+			var imgID = $img.attr('id');
+			var imgClass = $img.attr('class');
+			var imgURL = $img.attr('xlink:href');
+
+			jQuery.get(imgURL, function(data) {
+				// Get the SVG tag, ignore the rest
+				var $svg = jQuery(data).find('svg');
+
+				// Add replaced image's ID to the new SVG
+				if(typeof imgID !== 'undefined') {
+					$svg = $svg.attr('id', imgID);
+				}
+				// Add replaced image's classes to the new SVG
+				if(typeof imgClass !== 'undefined') {
+					$svg = $svg.attr('class', imgClass+' replaced-svg');
+				}
+
+				// Remove any invalid XML tags as per http://validator.w3.org
+				$svg = $svg.removeAttr('xmlns:a');
+
+				// Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+				if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+					$svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+				}
+
+				// Replace image with new SVG
+				$img.replaceWith($svg);
+
+			}, 'xml');
+
+		});
 	}
 }
