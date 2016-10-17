a186e01de938160b93f7b304e3d0b1bbec086ebd
Alexander <arikito.91@gmail.com>
2016-10-17 19:28:43 +0300

    jquery alias

--- systemjs.config.js
+++ systemjs.config.js
@@ -6,7 +6,8 @@
   System.config({
     paths: {
       // paths serve as alias
-      'npm:': 'node_modules/'
+      'npm:': 'node_modules/',
+      '@jquery': 'app/components/jquery/jquery.component',
     },
     // map tells the System loader where to look for things
     map: {
